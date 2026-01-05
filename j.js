4c31f80d68450549e3cdf86e879a6e1d9830690e
sadeelnedal264-debug <sadeelnedal264@gmail.com>
2026-01-06 00:45:36 +0300

    Update j.js

--- j.js
+++ j.js
@@ -15,7 +15,7 @@ const books = [
     { id: 1, title: "لِيطمئن قلبي", desc: "ادهم شرقاوي", price: 10, image: "https://via.placeholder.com/150x200?text=Book+1" },
     { id: 2, title: "رجال في الشمس", desc: "غسان كنغاني", price: 15, image: "https://via.placeholder.com/150x200?text=رجال+في+الشمس" },
     { id: 3, title: "موسم الهجره الى الشمال", desc: "الطيب صالح", price: 14, image: "https://via.placeholder.com/150x200?text=موسم+الهجرة+إلى+الشمال" },
-    { id: 4, title: "الولد", desc: "طه حسين", price: 22, image: "https://via.placeholder.com/150x200?text=الولد" },
+    { id: 4, title: "الولد", desc: "طه حسين", price: 22, image: "https://upload.wikimedia.org/wikipedia/ar/5/5c/Taha_Hussein.jpg" },
     { id: 5, title: "لا تطفئ الشمس", desc: "احسان عبد القدوس", price:20 , image: "https://via.placeholder.com/150x200?text=لا+تطفئ+الشمس" },
     { id: 6, title: "ليله القدر", desc: "نجيب محفوظ", price:16 , image: "https://via.placeholder.com/150x200?text=ليلة+القدر" },
     { id: 7, title: "الباب", desc: "يوسف سباعي", price: 25, image: "https://via.placeholder.com/150x200?text=الباب" },
